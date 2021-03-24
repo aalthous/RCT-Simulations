fdac99940a172c5a13c2b9daa9c4e71a7b33a344
aalthous <78035484+aalthous@users.noreply.github.com>
2021-03-24 16:11:30 -0400

    Update RCT_Time_To_Event_Outcome_RMST_Noninferiority.R

--- RCT_Time_To_Event_Outcome_RMST_Noninferiority.R
+++ RCT_Time_To_Event_Outcome_RMST_Noninferiority.R
@@ -9,12 +9,13 @@ set.seed(1)
 #Create Sample Trial Data
 pid=seq(1, by=1, len=nPerGroup*2)
 treatment=rep(1:2, nPerGroup)
-#recruitdate=floor((pid+1)/2)
 survivaltime <- numeric(nPerGroup*2)
-survivaltime[treatment==1] <- rweibull(nPerGroup, 1, 5000)
-survivaltime[treatment==2] <- rweibull(nPerGroup, 1, 5000) 
+survivaltime[treatment==1] <- rweibull(nPerGroup, 1, scale0)
+survivaltime[treatment==2] <- rweibull(nPerGroup, 1, scale1) 
 sampletrial=data.frame(cbind(pid, treatment, survivaltime))
-head(sampletrial, n=20)
+sampletrial$death<-ifelse(sampletrial$survivaltime <= 365, 1, 0)
+sampletrial$deathtime<-ifelse(sampletrial$death == 1,sampletrial$survivaltime,365)
+head(sampletrial, n=10)
 
 #Create Indicator Variables For Alive/Dead at Final Analysis (365 Days)
 
@@ -63,10 +64,15 @@ rmst_diff_ucl
 rmst_diff_pval=rmst_results$unadjusted.result[10]
 rmst_diff_pval
 
-#Store This Entire Code Sequence As Function; Repeat 100,000 Times
+#Store This Entire Code Sequence As Function
 
-nSims <- 1000
-nPerGroup <- 500 
+rm(trialresults)
+
+trial_simulation <- function(nSims, nPerGroup, scale0, scale1, seed) {
+
+set.seed(seed)
+
+trial<-numeric(nSims)
 hr <-numeric(nSims) 
 hr_lcl <-numeric(nSims) 
 hr_ucl <-numeric(nSims) 
@@ -74,23 +80,25 @@ hr_pvalue <-numeric(nSims)
 rmst_diff <-numeric(nSims) 
 rmst_diff_lcl <-numeric(nSims) 
 rmst_diff_ucl <-numeric(nSims) 
-rmst_diff_pvalue <-numeric(nSims) 
-
-set.seed(1)
+rmst_diff_pvalue <-numeric(nSims)
+ni_rmst_1 <-numeric(nSims)  
+ni_rmst_2 <-numeric(nSims)  
+ni_rmst_3 <-numeric(nSims)  
+ni_rmst_4 <-numeric(nSims)  
 
 for(i in 1:nSims){
+
+trial[i]=i
 
 #Create Sample Trial Data
 pid=seq(1, by=1, len=nPerGroup*2)
 treatment=rep(1:2, nPerGroup)
 survivaltime <- numeric(nPerGroup*2)
-survivaltime[treatment==1] <- rweibull(nPerGroup, 1, 10000)
-survivaltime[treatment==2] <- rweibull(nPerGroup, 1, 5000) 
+survivaltime[treatment==1] <- rweibull(nPerGroup, 1, scale0)
+survivaltime[treatment==2] <- rweibull(nPerGroup, 1, scale1) 
 sampletrial=data.frame(cbind(pid, treatment, survivaltime))
-sampletrial$deathdate=sampletrial$survivaltime
-sampletrial$death[sampletrial$deathdate<365]=1
-sampletrial$death[sampletrial$deathdate>=365]=0
-sampletrial$deathtime<-ifelse(sampletrial$death == 1,sampletrial$deathdate,365)
+sampletrial$death<-ifelse(sampletrial$survivaltime <= 365, 1, 0)
+sampletrial$deathtime<-ifelse(sampletrial$death == 1,sampletrial$survivaltime,365)
 
 #Cox Model
 coxmodel=coxph(Surv(deathtime, death) ~ treatment, data=sampletrial)
@@ -108,8 +116,56 @@ rmst_diff[i]=rmst_results$unadjusted.result[1]
 rmst_diff_lcl[i]=rmst_results$unadjusted.result[4]
 rmst_diff_ucl[i]=rmst_results$unadjusted.result[7]
 rmst_diff_pvalue[i]=rmst_results$unadjusted.result[10]
+ni_rmst_1[i]<-ifelse(rmst_diff_lcl[i] > - 7 , 1, 0)
+ni_rmst_2[i]<-ifelse(rmst_diff_lcl[i] > - 14 , 1, 0)
+ni_rmst_3[i]<-ifelse(rmst_diff_lcl[i] > - 21 , 1, 0)
+ni_rmst_4[i]<-ifelse(rmst_diff_lcl[i] > - 28 , 1, 0)
 
 }
 
-trialresults=cbind(hr, hr_lcl, hr_ucl, hr_pvalue, rmst_diff, rmst_diff_lcl, rmst_diff_ucl, rmst_diff_pvalue)
-head(trialresults, n=10)
+trialresults=as.data.frame(cbind(trial, 
+hr, hr_lcl, hr_ucl, hr_pvalue, 
+rmst_diff, rmst_diff_lcl, rmst_diff_ucl, rmst_diff_pvalue,
+ni_rmst_1, ni_rmst_2, ni_rmst_3, ni_rmst_4))
+
+return(trialresults)
+
+}
+
+trialresults1=trial_simulation(nSims=1000, nPerGroup=500, scale0=10000, scale1=10000, seed=1)
+table(trialresults1$ni_rmst_1)
+table(trialresults1$ni_rmst_2)
+table(trialresults1$ni_rmst_3)
+table(trialresults1$ni_rmst_4)
+
+trialresults2=trial_simulation(nSims=1000, nPerGroup=500, scale0=5000, scale1=5000, seed=1)
+table(trialresults2$ni_rmst_1)
+table(trialresults2$ni_rmst_2)
+table(trialresults2$ni_rmst_3)
+table(trialresults2$ni_rmst_4)
+
+trialresults3=trial_simulation(nSims=1000, nPerGroup=500, scale0=3000, scale1=3000, seed=1)
+table(trialresults3$ni_rmst_1)
+table(trialresults3$ni_rmst_2)
+table(trialresults3$ni_rmst_3)
+table(trialresults3$ni_rmst_4)
+
+trialresults4=trial_simulation(nSims=1000, nPerGroup=500, scale0=2000, scale1=2000, seed=1)
+table(trialresults4$ni_rmst_1)
+table(trialresults4$ni_rmst_2)
+table(trialresults4$ni_rmst_3)
+table(trialresults4$ni_rmst_4)
+
+trialresults5=trial_simulation(nSims=1000, nPerGroup=500, scale0=1000, scale1=1000, seed=1)
+table(trialresults5$ni_rmst_1)
+table(trialresults5$ni_rmst_2)
+table(trialresults5$ni_rmst_3)
+table(trialresults5$ni_rmst_4)
+
+
+
+
+
+
+
+
